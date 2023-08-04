f6cfa4d991dfc3c48ee7f93e93f03abbc338dc92
Shahadat Hossain Hridoy Al <71395891+HridoyHazard@users.noreply.github.com>
2023-08-04 21:30:32 +0600

    nodemon and concurrently added

---
+++ backend/data/meds.js
@@ -0,0 +1,95 @@
+const meds = [
+  {
+    _id:'1',
+    name: "B126 (Vitamin B1,B6,B12):",
+    image: "/images/vita.jpg",
+    description:
+      "Vitamin B1, B6 & B12 is indicated for the treatment of vitamin B1, B6 & B12 deficiency syndrome.It is also indicated for the supportive treatment of neuritis & non-inflammatory diseases of the nerves",
+    brand: "Popular Pharmaceuticals Ltd.",
+    price: 400,
+    category: "Pain",
+    rating:4.3,
+    numReviews:12,
+    countInStock: 10,
+  },
+  {
+    _id:'2',
+    name: "Paracetamol",
+    image: "/images/para.jpg",
+    description:
+      "Paracetamol is used to treat many conditions such as headache ,muscles aches ,arthritis, backache, toothaches, colds, and fevers. It relieves pain in mild arthritis but has no effect on the underlying inflammation and swelling of the joint.",
+    brand: "Square Pharmaceuticals Ltd.",
+    price: 450,
+    category: "Pain",
+    rating:4.3,
+    numReviews:12,
+    countInStock: 0,
+  },
+  {
+    _id:'3',
+    name: "Cabergoline",
+    image: "/images/Cabergol.jpg",
+    description:
+      "It is indicated for the treatment of hyperprolactinemic disorders, either idiopathic or due to pituitary adenomas. It is used to stop breast milk production (lactation) soon after childbirth, stillbirth, abortion or miscarriage. It can also be used to treat other conditions caused by hormonal disturbance which can result in high levels of prolactin being produced. This includes high levels of prolactin caused by tumours of the pituitary gland in both men and women.",
+    brand: "Popular Pharmaceuticals Ltd.",
+    price: 80, 
+    category: "Pain",
+    rating:4.3,
+    numReviews:12,
+    countInStock: 5,
+  },
+  {
+    _id:'4',
+    name: "Civox 500mg",
+    image: "/images/Civox.jpg",
+    description:
+      "Ciprofloxacin is indicated for the treatment of Respiratory Tract Infections,Urinary tract infections, Pelvic Inflammatory Diseases, Infectious Diarrhea (Shigella dysenteriae, Vibrio cholera), Typhoid fever, Intra-abdominal infections, Prostatitis, Skin and Soft Tissue Infections, Bone and Joint Infections, Gonorrhea, Neutropenic patients with fever due to bacterial infection, Meningitis, Surgical prophylaxis.",
+    brand: "Popular Pharmaceuticals Ltd.",
+    price: 337,
+    category: "Pain",
+    rating:4.3,
+    numReviews:12,
+    countInStock: 11,
+  },
+  {
+    _id:'5',
+    name: "Norium 5 mg",
+    image: "/images/Norium.jpg",
+    description:
+      "Prophylaxis of classic (with aura) or common (without aura) migraine, Symptomatic treatment of vestibular vertigo (due to a diagnosed functional disorder of the vestibular system).Peripheral Vascular Disease (PVD),Motion sickness,Refractory epilepsy resistant to conventional antiepileptic therapy.",
+    brand: "Eskayef Pharmaceuticals Ltd.",
+    price: 400,
+    category: "Pain",
+    rating:4.3,
+    numReviews:12,
+    countInStock: 7,
+  },
+  {
+    _id:'6',
+    name: "Sergel 20mg",
+    image: "/images/Sergel.jpg",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    brand: "Healthcare Pharmaceuticals Ltd.",
+    price: 700,
+    category: "Relax",
+    rating:4.3,
+    numReviews:12,
+    countInStock: 0,
+  },
+  {
+    _id:'7',
+    name: "Alatrol Tablet 10 mg",
+    image: "/images/Alatrol.jpg",
+    description:
+      "It is indicated for the relief of symptoms associated with seasonal & perennial allergic rhinitis. It is also indicated for the treatment of the uncomplicated skin manifestations of chronic idiopathic urticaria and allergen induced asthma.",
+    brand: "Square Pharmaceuticals Ltd.",
+    price: 452,
+    category: "Pain",
+    rating:4.3,
+    numReviews:12,
+    countInStock: 7,
+  },
+];
+
+export default meds;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import meds from "./data/meds.js";
 
 const port = 5000;
 
@@ -7,5 +8,14 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Api is running...");
 });
+
+app.get('/api/meds', (req,res) => {
+  res.json(meds);
+})
+
+app.get('/api/meds/:id', (req,res) => {
+  const med = meds.find((p) => p._id === req.params.id)
+  res.json(med);
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
