170f8485f56da2e45d97e71ab2f34739bb4f8933
Shahadat Hossain Hridoy Al <71395891+HridoyHazard@users.noreply.github.com>
2023-08-25 23:53:38 +0600

    login screen added & few patch up

---
+++ frontend/src/Screens/LoginScreen.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="my-2" controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          New Customer? <Link to="/register">Register</Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
