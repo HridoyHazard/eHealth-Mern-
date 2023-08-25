cb638a019342c7d846d843ef626bb246c4883a4c
Shahadat Hossain Hridoy Al <71395891+HridoyHazard@users.noreply.github.com>
2023-08-25 21:09:23 +0600

    logout user & clear cookie

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -44,7 +44,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 };
 
 // @desc    Get user profile
