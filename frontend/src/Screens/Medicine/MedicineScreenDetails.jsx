e83f1ba11864cefac22a03ea793169bf22d2b667
Shahadat Hossain Hridoy Al <71395891+HridoyHazard@users.noreply.github.com>
2023-10-05 20:48:10 +0600

    favicon added

--- frontend/src/Screens/Medicine/MedicineScreenDetails.jsx
+++ frontend/src/Screens/Medicine/MedicineScreenDetails.jsx
@@ -8,6 +8,7 @@ import {
   Button,
   Image,
   Form,
+  Container,
 } from "react-bootstrap";
 import Rating from "../../components/Rating";
 import Loader from "../../components/Loader";
@@ -54,7 +55,7 @@ const MedicineScreenDetails = () => {
         comment,
       }).unwrap();
       refetch();
-      toast.success('Review created successfully');
+      toast.success("Review created successfully");
     } catch (err) {
       toast.error(err?.data?.message || err.error);
     }
@@ -78,137 +79,140 @@ const MedicineScreenDetails = () => {
         </Message>
       ) : (
         <>
-          <Row>
-            <Col md={6}>
-              <Image src={med.image} alt={med.name} fluid />
-            </Col>
-            <Col md={3}>
-              <ListGroup variant="flush">
-                <ListGroup.Item>
-                  <h3>{med.name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Rating
-                    value={med.rating}
-                    text={`${med.numReviews} reviews`}
-                  />
-                </ListGroup.Item>
-                <ListGroupItem>
-                  <strong>৳{med.price}</strong>
-                </ListGroupItem>
-                <ListGroupItem>
-                  <strong>{med.description}</strong>
-                </ListGroupItem>
-              </ListGroup>
-            </Col>
-            <Col md={3}>
-              <Card>
+          <Container>
+            <Row>
+              <Col md={6}>
+                <Image src={med.image} alt={med.name} fluid />
+              </Col>
+              <Col md={3}>
                 <ListGroup variant="flush">
                   <ListGroup.Item>
-                    <Row>
-                      <Col>Status:</Col>
-                      <Col>
-                        {med.countInStock > 0 ? "In Stock" : "Out Of Stock"}
-                      </Col>
-                    </Row>
+                    <h3>{med.name}</h3>
                   </ListGroup.Item>
-                  {med.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Rating
+                      value={med.rating}
+                      text={`${med.numReviews} reviews`}
+                    />
+                  </ListGroup.Item>
+                  <ListGroupItem>
+                    <strong>৳{med.price}</strong>
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <p>{med.description}</p>
+                  </ListGroupItem>
+                </ListGroup>
+              </Col>
+              <Col md={3}>
+                <Card>
+                  <ListGroup variant="flush">
                     <ListGroup.Item>
                       <Row>
-                        <Col>Qty</Col>
+                        <Col>Status:</Col>
                         <Col>
-                          <Form.Control
-                            as="select"
-                            value={qty}
-                            onChange={(e) => setQty(Number(e.target.value))}
-                          >
-                            {[...Array(med.countInStock).keys()].map((x) => (
-                              <option key={x + 1} value={x + 1}>
-                                {x + 1}
-                              </option>
-                            ))}
-                          </Form.Control>
+                          {med.countInStock > 0 ? "In Stock" : "Out Of Stock"}
                         </Col>
                       </Row>
                     </ListGroup.Item>
-                  )}
+                    {med.countInStock > 0 && (
+                      <ListGroup.Item>
+                        <Row>
+                          <Col>Qty</Col>
+                          <Col>
+                            <Form.Control
+                              as="select"
+                              value={qty}
+                              onChange={(e) => setQty(Number(e.target.value))}
+                            >
+                              {[...Array(med.countInStock).keys()].map((x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              ))}
+                            </Form.Control>
+                          </Col>
+                        </Row>
+                      </ListGroup.Item>
+                    )}
+                    <ListGroup.Item>
+                      <Button
+                        className="btn-block"
+                        type="button"
+                        disabled={med.countInStock === 0}
+                        onClick={addToCartHandler}
+                      >
+                        Add To Cart
+                      </Button>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card>
+              </Col>
+            </Row>
+            <Row className="review">
+              <Col md={6}>
+                <h2>Reviews</h2>
+                {med.reviews.length === 0 && <Message>No Reviews</Message>}
+                <ListGroup variant="flush">
+                  {med.reviews.map((review) => (
+                    <ListGroup.Item key={review._id}>
+                      <strong>{review.name}</strong>
+                      <Rating value={review.rating} />
+                      <p>{review.createdAt.substring(0, 10)}</p>
+                      <p>{review.comment}</p>
+                    </ListGroup.Item>
+                  ))}
                   <ListGroup.Item>
-                    <Button
-                      className="btn-block"
-                      type="button"
-                      disabled={med.countInStock === 0}
-                      onClick={addToCartHandler}
-                    >
-                      Add To Cart
-                    </Button>
+                    <h2>Write a Customer Review</h2>
+
+                    {loadingMedicineReview && <Loader />}
+
+                    {userInfo ? (
+                      <Form onSubmit={submitHandler}>
+                        <Form.Group className="my-2" controlId="rating">
+                          <Form.Label>Rating</Form.Label>
+                          <Form.Control
+                            as="select"
+                            required
+                            value={rating}
+                            onChange={(e) => setRating(e.target.value)}
+                          >
+                            <option value="">Select...</option>
+                            <option value="1">1 - Poor</option>
+                            <option value="2">2 - Fair</option>
+                            <option value="3">3 - Good</option>
+                            <option value="4">4 - Very Good</option>
+                            <option value="5">5 - Excellent</option>
+                          </Form.Control>
+                        </Form.Group>
+                        <Form.Group className="my-2" controlId="comment">
+                          <Form.Label>Comment</Form.Label>
+                          <Form.Control
+                            as="textarea"
+                            row="3"
+                            required
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
+                          ></Form.Control>
+                        </Form.Group>
+                        <Button
+                          disabled={loadingMedicineReview}
+                          type="submit"
+                          variant="primary"
+                        >
+                          Submit
+                        </Button>
+                      </Form>
+                    ) : (
+                      <Message>
+                        Please <Link to="/login">sign in</Link> to write a
+                        review
+                      </Message>
+                    )}
                   </ListGroup.Item>
                 </ListGroup>
-              </Card>
-            </Col>
-          </Row>
-          <Row className="review">
-            <Col md={6}>
-              <h2>Reviews</h2>
-              {med.reviews.length === 0 && <Message>No Reviews</Message>}
-              <ListGroup variant="flush">
-                {med.reviews.map((review) => (
-                  <ListGroup.Item key={review._id}>
-                    <strong>{review.name}</strong>
-                    <Rating value={review.rating} />
-                    <p>{review.createdAt.substring(0, 10)}</p>
-                    <p>{review.comment}</p>
-                  </ListGroup.Item>
-                ))}
-                <ListGroup.Item>
-                  <h2>Write a Customer Review</h2>
-
-                  {loadingMedicineReview && <Loader />}
-
-                  {userInfo ? (
-                    <Form onSubmit={submitHandler}>
-                      <Form.Group className="my-2" controlId="rating">
-                        <Form.Label>Rating</Form.Label>
-                        <Form.Control
-                          as="select"
-                          required
-                          value={rating}
-                          onChange={(e) => setRating(e.target.value)}
-                        >
-                          <option value="">Select...</option>
-                          <option value="1">1 - Poor</option>
-                          <option value="2">2 - Fair</option>
-                          <option value="3">3 - Good</option>
-                          <option value="4">4 - Very Good</option>
-                          <option value="5">5 - Excellent</option>
-                        </Form.Control>
-                      </Form.Group>
-                      <Form.Group className="my-2" controlId="comment">
-                        <Form.Label>Comment</Form.Label>
-                        <Form.Control
-                          as="textarea"
-                          row="3"
-                          required
-                          value={comment}
-                          onChange={(e) => setComment(e.target.value)}
-                        ></Form.Control>
-                      </Form.Group>
-                      <Button
-                        disabled={loadingMedicineReview}
-                        type="submit"
-                        variant="primary"
-                      >
-                        Submit
-                      </Button>
-                    </Form>
-                  ) : (
-                    <Message>
-                      Please <Link to="/login">sign in</Link> to write a review
-                    </Message>
-                  )}
-                </ListGroup.Item>
-              </ListGroup>
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+          </Container>
         </>
       )}
     </>
