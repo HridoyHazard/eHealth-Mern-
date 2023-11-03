b37e2476c546dfcd60f4aa4a83eeb11aa807f4bf
Shahadat Hossain Hridoy Al <71395891+HridoyHazard@users.noreply.github.com>
2023-11-03 20:01:47 +0600

    user verified added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -12,18 +12,24 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  if (user && (await user.matchPassword(password))) {
-    generateToken(res, user._id);
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    });
+  if (user.isVerified === true) {
+    if (user && (await user.matchPassword(password))) {
+      generateToken(res, user._id);
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
   } else {
     res.status(401);
-    throw new Error("Invalid email or password");
+    throw new Error("Please active your account");
   }
+
   res.send("Auth user");
 });
 
